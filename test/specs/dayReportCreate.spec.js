9fa7c57e1dac0de685fc6a2c88f5140d65ed9fc5
Roman Vorobey <58947142+rvorobey@users.noreply.github.com>
2020-02-05 13:48:29 -0500

    added XPath selectors

--- test/specs/dayReportCreate.spec.js
+++ test/specs/dayReportCreate.spec.js
@@ -4,58 +4,58 @@ const { URL_DIARY } = require('./register.data');
 const { selectors } = require('./register.data');
 
 describe('CREATE DAY REPORT', () => {
-  before('Login as admin', () => {
+  before('Login as [ADMIN]', () => {
     browser.url(URL_LOGIN);
     $('form input[name="email"]').setValue('test2020@test.com');
     $('form input[name="password"]').setValue('qwerty');
     $('form button[type="submit"]').click();
     browser.pause(1000);
   });
-  it('should open Diary page', () => {
+  it('should open [DIARY] page', () => {
     browser.url(URL_DIARY);
     browser.pause(1000);
   });
-  it('should have correct header', () => {
+  it('should have correct [HEADER]', () => {
     const actual = $(selectors.h1).getText();
     const expected = 'Daily reports';
     expect(actual).equal(expected);
   });
-  it('should click create day report', () => {
+  it('should click [CREATE DAY REPORT]', () => {
     $(selectors.createButton).click();
   });
-  it('should display get last 50 message', () => {
+  it('should display get [LAST 50 MESSAGES]', () => {
     const actual = $(selectors.h4).getText();
     const expected = 'Get last (50) day reports';
     expect(actual).equal(expected);
   });
-  it('should have correct header', () => {
+  it('should have correct [HEADER]', () => {
     const actual = $(selectors.h1).getText();
     const expected = 'Create day report';
     expect(actual).equal(expected);
   });
-  it('should check [i need help] box', () => {
+  it('should check [I NEED HELP] box', () => {
     const element = $(selectors.report.help);
     element.click();
     browser.pause(1000);
   });
-  it('should check [i understood everything] box', () => {
+  it('should check [I UNDERSTOOD EVERYTHING] box', () => {
     const element = $(selectors.report.understood);
     element.click();
     browser.pause(1000);
   });
-  it('should select [morale level]', () =>{
+  it('should select [MORALE LEVEL]', () =>{
     const element = $(selectors.report.morale);
     element.selectByVisibleText('7');
     browser.pause(1000);
   });
-  it('should fill out [how many hours] form', () => {
+  it('should fill out [HOW MANY HOURS] form', () => {
     $(selectors.report.hours).setValue('5');
   });
-  it('should fill out [how was your day] field', () => {
+  it('should fill out [HOW WAS YOUR DAY] field', () => {
     $(selectors.report.day).setValue('Must be 30 characters or more!!!!!!');
     browser.pause(3000);
   });
-  it('should click [save] button', () => {
+  it('should click [SAVE] button', () => {
     $(selectors.report.save).click();
     browser.pause(3000);
   });
